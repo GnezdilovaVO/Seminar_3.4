275734af8bdae689497277cd227500340827f0f2
Anton Vorontsov <antvork@gmail.com>
2023-03-06 14:12:51 +0700

    Решили задачу со *

--- Example_001/Program.cs
+++ Example_001/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿int[] arr = { 1, 0, 1, 0, 0 };
+double number = 0;
+int a = arr.Length - 1;
+for (int i = 0; i < arr.Length; i++)
+{
+    number = number + arr[i] * Math.Pow(2, a);
+    a = a - 1;
+}
+Console.WriteLine(number);
