442fab28e5f41ba115df8d35b28f4be7878a11bd
Anton Vorontsov <antvork@gmail.com>
2023-03-06 14:59:14 +0700

    add zask 4

--- Example_002/Program.cs
+++ Example_002/Program.cs
@@ -1,2 +1,17 @@
 ﻿Console.WriteLine("Введите число:");
-int N = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n <= 0)
+{
+    Console.WriteLine("Число не соответсвует задаче");
+}
+else
+{
+
+}
+
+for (int index=0; index <= arr.Length-1; index++)
+{
+    arr[index] = new Random().Next(0, 1);
+}
+Console.WriteLine(arr);
